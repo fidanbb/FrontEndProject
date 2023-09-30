e4b6e13f716c0815a4ec9d92624c04d27c2c6055
Fidan Bashirova <fidanbb@code.edu.az>
2023-09-30 22:58:42 +0400

    added products slider,and 3 more sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //header js
   $(".lang a").hover(function (e) {
     e.preventDefault();
     $(".lang-items").toggleClass("d-none");
@@ -70,7 +71,7 @@ $(document).ready(function () {
     $(".search-items").addClass("d-none");
   });
 
-  //slider
+  //slider section
   const swiper = new Swiper(".slider-swiper", {
     loop: true,
     pagination: {
@@ -88,22 +89,75 @@ $(document).ready(function () {
     $("#slider .swiper-button-next").toggleClass("d-none");
   });
 
+  //products section
   var swiperProducts = new Swiper(".swiper-products", {
     loop: true,
-    slidesPerView: 3, // Display 4 slides at a time
-    slidesPerGroup: 3, // Navigate one slide at a time
-    spaceBetween: 40, // Adjust this value as needed
-
+    // slidesPerView: 4,
+    // slidesPerGroup: 4,
+    // spaceBetween: 20,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 20,
+      },
+      766: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 20,
+      },
+      300: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30,
+      },
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+
+  $(".swinger-container").swinger();
+
+  $(".swiper-slide").hover(function () {
+    let dataId = $(this).data("swiper-slide-index");
+
+    $(".product-icons").each(function () {
+      if ($(this).data("swiper-slide-index") === dataId) {
+        $(this).toggleClass("transform-product-icons");
+      }
+    });
+  });
+
+  // Brands Slider part
+
+  const brandsSwiper = new Swiper(".brands-swiper", {
+    loop: true,
+    slidesPerView: 5,
+    slidesPerGroup: 2,
+    spaceBetween: 10,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+  });
+
+  // $(".brands-slide").slick({
+  //   infinite: true,
+  //   speed: 200,
+  //   slidesToShow: 5,
+  //   // centerMode: true,
+  //   // variableWidth: true,
+  // });
 
   $($("body")).click(function () {
     if (!$(".social-media-items").hasClass("d-none")) {
