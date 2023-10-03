440d7acc14924b345f9f8eae2d99662aaa4e0aaf
Fidan Bashirova <fidanbb@code.edu.az>
2023-10-03 20:40:16 +0400

    shope page half of it done

---
+++ assets/js/shop.js
@@ -0,0 +1,182 @@
+$(document).ready(function () {
+  // header js
+
+  $(".lang a").hover(function (e) {
+    e.preventDefault();
+    $(".lang-items").toggleClass("d-none");
+  });
+
+  $(".currency a").hover(function (e) {
+    e.preventDefault();
+
+    $(".currency-items").toggleClass("d-none");
+  });
+
+  $(".lang-items").hover(function () {
+    $(this).toggleClass("d-none");
+  });
+
+  $(".currency-items").hover(function () {
+    $(this).toggleClass("d-none");
+  });
+
+  $(".social-media a").click(function (e) {
+    e.preventDefault();
+
+    $(".social-media-items").toggleClass("d-none");
+    e.stopPropagation();
+  });
+
+  $(".open-sidebar").click(function (e) {
+    $(".menu-sidebar").removeClass("transform-sidebar");
+    $(".sidebar-overlay").removeClass("d-none");
+    e.stopPropagation();
+  });
+
+  $(".sidebar-closer i").click(function (e) {
+    $(".menu-sidebar").addClass("transform-sidebar");
+    $(".sidebar-overlay").addClass("d-none");
+    e.stopPropagation();
+  });
+
+  $(".open-home").hover(function (e) {
+    e.preventDefault();
+    $(".home-items").toggleClass("d-none");
+  });
+
+  $(".home-items").hover(function () {
+    $(this).toggleClass("d-none");
+  });
+
+  $(".open-shop").hover(function (e) {
+    e.preventDefault();
+    $(".shop-items").toggleClass("d-none");
+  });
+
+  $(".shop-items").hover(function () {
+    $(this).toggleClass("d-none");
+  });
+
+  $(".open-search").click(function (e) {
+    e.preventDefault();
+    $(this).addClass("d-none");
+    $(".close-search").removeClass("d-none");
+    $(".search-items").removeClass("d-none");
+    $("main").addClass("d-none");
+    $("footer").addClass("d-none");
+  });
+
+  $(".close-search2").click(function (e) {
+    e.preventDefault();
+    $(".open-search").removeClass("d-none");
+    $(".close-search").addClass("d-none");
+    $(".search-items").addClass("d-none");
+  });
+
+  //   Shop js
+
+  $(".categories-item label span").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("span-click");
+    $(this).parent().toggleClass("label-click");
+  });
+
+  // $(".categories-item label").click(function (e) {
+  //   e.preventDefault();
+  //   $(this).toggleClass("label-click");
+  //   $(this).children().toggleClass("span-click");
+  // });
+
+  $(".subDropdown.plus").click(function () {
+    $(this).addClass("d-none");
+    $(this).next().removeClass("d-none");
+    let dataId = $(this).data("id");
+    console.log(dataId);
+    $(".children").each(function () {
+      if ($(this).data("id") === dataId) {
+        $(this).removeClass("d-none");
+      }
+    });
+  });
+
+  $(".subDropdown.minus").click(function () {
+    $(this).addClass("d-none");
+    $(this).prev().removeClass("d-none");
+    $(".children").addClass("d-none");
+  });
+
+  // slider range js
+
+  let min = 10;
+  let max = 100;
+
+  const calcLeftPosition = (value) => (100 / (100 - 10)) * (value - 10);
+
+  $("#rangeMin").on("input", function (e) {
+    const newValue = parseInt(e.target.value);
+    if (newValue > max) return;
+    min = newValue;
+    $("#thumbMin").css("left", calcLeftPosition(newValue) + "%");
+    $("#min").html(newValue);
+    $("#line").css({
+      left: calcLeftPosition(newValue) + "%",
+      right: 100 - calcLeftPosition(max) + "%",
+    });
+  });
+
+  $("#rangeMax").on("input", function (e) {
+    const newValue = parseInt(e.target.value);
+    if (newValue < min) return;
+    max = newValue;
+    $("#thumbMax").css("left", calcLeftPosition(newValue) + "%");
+    $("#max").html(newValue);
+    $("#line").css({
+      left: calcLeftPosition(min) + "%",
+      right: 100 - calcLeftPosition(newValue) + "%",
+    });
+  });
+
+  // filter by size checbox js
+
+  $(".filter-by-size .size-type span").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("span-click");
+    $(this).next().toggleClass("a-click");
+  });
+
+  $(".filter-by-size .size-type a").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("a-click");
+    $(this).prev().toggleClass("span-click");
+  });
+
+  // product-status checkbox js
+
+  $(".product-status .status-type span").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("span-click");
+    $(this).next().toggleClass("a-click");
+  });
+
+  $(".product-status .status-type a").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("a-click");
+    $(this).prev().toggleClass("span-click");
+  });
+
+  // body js
+
+  $($("body")).click(function () {
+    if (!$(".social-media-items").hasClass("d-none")) {
+      $(".social-media-items").addClass("d-none");
+    }
+
+    if (
+      !$(".menu-sidebar").hasClass("transform-sidebar") ||
+      !$(".social-media-items").hasClass("d-none")
+    ) {
+      $(".menu-sidebar").addClass("transform-sidebar");
+      $(".sidebar-overlay").addClass("d-none");
+    }
+  });
+});
